d6a3970e05afae41cb7f1d4bb3a8de5ece5d873f
Andrew Engebretson <engebre@gmail.com>
2025-05-10 09:48:31 -0500

    Create j_interpreter_lib.hpp

---
+++ src/j_interpreter_lib.hpp
@@ -0,0 +1,12 @@
+#ifndef J_INTERPRETER_LIB_HPP
+#define J_INTERPRETER_LIB_HPP
+
+// Include the main public interfaces of your library
+#include "common/source_location.hpp"
+#include "lexer/token.hpp"
+#include "lexer/lexer.hpp"
+#include "ast/ast_nodes.hpp"
+#include "parser/parser.hpp"
+#include "interpreter/interpreter.hpp" // When it's ready
+
+#endif // J_INTERPRETER_LIB_HPP
